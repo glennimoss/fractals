fe6725827c04f0f9863b7f5ec2b096ff577a141c
Glenn Moss <gim@keynetics.com>
2012-05-16 17:10:19 -0600

    Added another rejection strategy

--- curve_builder.py
+++ curve_builder.py
@@ -17,6 +17,24 @@ def inv (dir):
     return right
   if dir is right:
     return left
+def l (dir):
+  if dir is up:
+    return left
+  if dir is left:
+    return down
+  if dir is down:
+    return right
+  if dir is right:
+    return up
+def r (dir):
+  if dir is up:
+    return right
+  if dir is right:
+    return down
+  if dir is down:
+    return left
+  if dir is left:
+    return up
 
 top_left = numpy.array((-1,-1))
 top_right = numpy.array((1,-1))
@@ -35,12 +53,16 @@ def walk (size):
   end_pos = (end_x, end_y)
   #print("today's end_pos is", end_pos)
 
+  def at (pos, default=None):
+    x, y = pos
+    if 0 <= x <= edge and 0 <= y <= edge:
+      return shape[y][x]
+    return default
 
   def edges (*pos, lax=False):
     if len(pos) == 1:
       pos = pos[0]
-    x, y = pos
-    cur = shape[y][x]
+    cur = at(pos)
     edges = []
 
     #if not lax:
@@ -49,21 +71,27 @@ def walk (size):
     for c in all_dirs:
       try:
         x, y = pos + c
-        if x >= 0 and y >= 0 and shape[y][x] == 0:
+        c_pos = (x,y)
+        if at(c_pos) == 0:
           if not lax:
-            if (x,y) == end_pos and cur + 1 != size**2:
+            if c_pos == end_pos and cur + 1 != size**2:
               #print('rejected', c, 'for being too early')
               continue
-            if x in (0, edge) and y != 0 and shape[y-1][x] == 0:
+            if x in (0, edge) and y != 0 and at(c_pos + up) == 0:
               #print('rejected', c, 'for cutting off above')
               continue
-            if y == 0 and shape[y][x-1] == 0:
+            if y == 0 and at(c_pos + left) == 0:
               #print('rejected', c,' for cutting off top left')
               continue
             if (y == edge and x not in (0, edge) and
-                ((end_x == 0 and shape[y][x+1] == 0) or
-                 (end_x == edge and shape[y][x-1] == 0))):
+                ((end_x == 0 and at(c_pos + right) == 0) or
+                 (end_x == edge and at(c_pos + left) == 0))):
               #print('rejected', c, 'for cutting off side')
+              continue
+            f = c_pos + c
+            if (at(c_pos + l(c), 0) == 0 and at(c_pos + r(c), 0) == 0 and
+                (at(f,0) != 0 or at(f + l(c),0) != 0 or at(f + r(c),0) != 0)):
+              #print('rejected', c, 'for creating a closed shape')
               continue
           edges.append(c)
           #if not lax:
@@ -74,9 +102,10 @@ def walk (size):
     return edges
 
   failed_steps = 0
+  logged = False
 
   def walk (pos, seq=1):
-    nonlocal failed_steps
+    nonlocal failed_steps, logged
     x, y = pos
     shape[y][x] = seq
     if ((x,y) == end_pos and seq == size**2):
@@ -93,17 +122,28 @@ def walk (size):
         dirs = forced_dirs
     random.shuffle(dirs)
 
+    #if logged:
+      #print('Backtracked to:')
+      #print(shape)
+    logged = False
     for c in dirs:
       if walk(pos + c, seq + 1):
         return True
 
     #print('FAILED! backtracking...')
-    failed_steps += 1
+    if not logged:
+      #print(shape)
+      failed_steps += 1
+      logged = True
     x, y = pos
     shape[y][x] = 0
 
-  walk((0,0))
-  print(failed_steps, 'failed steps')
+  try:
+    walk((0,0))
+  except KeyboardInterrupt:
+    pass
+
+  print(failed_steps, 'failed paths')
 
   return shape
 
@@ -164,7 +204,7 @@ def trace_curve (shape):
   prev_dir = down
   script = ''
 
-  curve = [['' for x in range(len(shape))] for y in range(len(shape))]
+  curve = [[' ' for x in range(len(shape))] for y in range(len(shape))]
   while True:
     dir = next_dir(x,y)
     if dir is None:
@@ -195,4 +235,5 @@ if __name__ == '__main__':
   for row in curve:
     for c in row:
       print(c, end='')
-    print()
+    print('|')
+  print('-'*shape_size)
